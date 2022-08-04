5dc4e9a7dabee0e61eee5e1366a87ab2e46d7b99
QuentinHumbert <quentinhum76@gmail.com>
2022-08-04 13:50:44 +0200

    Auth done

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -35,7 +35,7 @@ class MainController extends Controller
                 $userArray = $userModel->findOneByUsername(strip_tags($_POST['username']));
                 if(!$userArray) {
                     $_SESSION['erreur'] = 'Le pseudo et/ou le mot de passe est incorrect.';
-                    header('Location: /user/login');
+                    header('Location: /main/login');
                     exit;
                 }
                 
@@ -70,7 +70,56 @@ class MainController extends Controller
 
     public function register()
     {
-        $this->twig->display('main/register.html.twig');
+        // if(isset($_SESSION['user'])) {
+        //     header('Location: /');
+        //     exit;
+        // }
+
+        if(!empty($_POST)) {
+                $lastname = strip_tags($_POST['lastname']);
+                $firstname = strip_tags($_POST['firstname']);
+                $username = strip_tags($_POST['username']);
+                $email = strip_tags($_POST['email']);
+                $password = password_hash($_POST['password'], PASSWORD_ARGON2I);
+                
+                $user = new UsersModel;
+                $user->setLastname($lastname)
+                    ->setFirstname($firstname)
+                    ->setUsername($username)
+                    ->setEmail($email)
+                    ->setPassword($password);
+    
+                $user->create();
+                header('Location: /');
+                exit;
+        } 
+        $lastname = isset($_POST['lastname']) ? strip_tags($_POST['lastname']) : '';
+        $firstname = isset($_POST['firstname']) ? strip_tags($_POST['firstname']) : '';
+        $username = isset($_POST['username']) ? strip_tags($_POST['firstname']) : '';
+        $email = isset($_POST['email']) ? strip_tags($_POST['email']) : '';
+        
+        
+        $form = new Form;
+
+        $form->debutForm('post', '#', ['enctype' => 'multipart/form-data'])
+            ->ajoutLabelFor('lastname', 'Votre nom :')
+            ->ajoutInput('text', 'lastname', ['id' => 'lastname', 'class' => '', 'placeholder' => 'Nom', 'value' => $lastname])
+            ->ajoutLabelFor('firstname', 'Votre prénom :')
+            ->ajoutInput('text', 'firstname', ['id' => 'firstname', 'class' => '', 'placeholder' => 'Prénom', 'value' => $firstname])
+            ->ajoutLabelFor('username', 'Votre pseudo :')
+            ->ajoutInput('text', 'username', ['id' => 'username', 'class' => '', 'placeholder' => 'Prénom', 'value' => $username])
+            ->ajoutLabelFor('email', 'Votre email :')
+            ->ajoutInput('email', 'email', ['id' => 'email', 'class' => '', 'placeholder' => 'Adresse email', 'value' => $email])
+            ->ajoutLabelFor('password', 'Votre mot de passe :')
+            ->ajoutInput('password', 'password', ['id' => 'password', 'class' => '', 'placeholder' => 'Mot de passe'])
+            ->ajoutBouton('M\'enregistrer', ['class' => ''])
+            ->finForm();
+
+            $this->twig->display('main/register.html.twig', [
+            'registerForm' => $form->create(),
+            'message' => isset($_SESSION['erreur']) ? $_SESSION['erreur'] : ''
+        ]);
+        unset($_SESSION['erreur']);
     }
 
     
